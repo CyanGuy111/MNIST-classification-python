a116a4ea64f0bd7349bd6b3acebb0705eaed50ab
CyanGuy111 <46373872+CyanGuy111@users.noreply.github.com>
2024-11-20 11:26:20 +0700

    help

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-import csv
-import random
-import time
 from libs.variables import *
-from libs.matCalc import *
-from libs.dirName import *
-from libs.fileReading import *
-from libs.mathFunc import * 
-from libs.weightBiasGen import *
-from libs.forward import *
 from libs.training import *
 from libs.testing import *
 
